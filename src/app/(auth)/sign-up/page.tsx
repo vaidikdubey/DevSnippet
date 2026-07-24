fcebb271c749437eed381a5cc518a44ce08d55d5
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-24 16:23:20 +0530

    Added shadcn components

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,24 @@
 'use client'
 
+//Shadcn components
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+    FieldGroup,
+    Field,
+    FieldLabel,
+    FieldError,
+} from "@/components/ui/field";
+import { Loader2 } from "lucide-react";
+
 const SignInPage = () => {
   return (
     <div>SignInPage</div>
