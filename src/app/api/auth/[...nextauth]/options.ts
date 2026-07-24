130163e420d9e362f916cdc1e6c13b7120dca786
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-24 15:29:43 +0530

    Added nextauth sign-in

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,61 @@
+import dbConnect from "@/app/lib/dbConnect";
+import UserModel from "@/app/model/User";
+import bcrypt from "bcryptjs";
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) { 
+                await dbConnect()
+
+                try {
+                    const user = await UserModel.findOne({ email: credentials?.email });
+
+                    if (!user) throw new Error("User not found");
+
+                    const enteredPassword = credentials?.password || ""
+                    const isPasswordCorrect = await bcrypt.compare(enteredPassword, user.password)
+
+                    if(isPasswordCorrect) {
+                        return {
+                            id: user._id.toString(),
+                            name: user.name,
+                            email: user.email,
+                        }
+                    }
+                    else throw new Error("Invalid credentials")
+                } catch (error) {
+                    throw new Error(error instanceof Error ? error.message : "Auth failed")
+                }
+            }
+        })
+    ],
+
+    callbacks: {
+        async jwt({ token }) { 
+            return token
+        },
+
+        async session({ session }) { 
+            return session;
+        }
+    },
+
+    pages: {
+        signIn: "/sign-in"
+    },
+
+    session: {
+        strategy: "jwt"
+    },
+
+    secret: process.env.NEXTAUTH_SECRET
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { NextAuthOptions, User } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth/next";
+import { authOptions } from "./options";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
