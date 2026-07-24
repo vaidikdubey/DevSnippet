d8de5c6ceb48a59a55a1f3fc0313e896986e2aa9
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-24 20:33:33 +0530

    Added proxy

---
+++ src/proxy.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt"
+
+export async function proxy(request: NextRequest) { 
+    const token = await getToken({ req: request })
+    const url = request.nextUrl
+
+    if (token && (
+        url.pathname === "/" ||
+        url.pathname.startsWith("/sign-in") ||
+        url.pathname.startsWith("/sign-up")
+    )) { 
+        return NextResponse.redirect(new URL("/dashboard", request.url))
+    }
+
+    if(!token && url.pathname.startsWith("/dashboard")) return NextResponse.redirect(new URL("/sign-in", request.url))
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: [
+        "/sign-in",
+        "/sign-up",
+        "/",
+        "/dashboard/:path*",
+        "/create"
+    ]
+}
