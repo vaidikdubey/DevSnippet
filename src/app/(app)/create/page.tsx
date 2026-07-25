b9c17aa69b79e1a1c39d10c8e8e70ae757edcd8c
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-25 15:23:01 +0530

    Updated create snippet page to use monaco editor and custom styling

--- src/app/(app)/create/page.tsx
+++ src/app/(app)/create/page.tsx
@@ -4,36 +4,25 @@ import { toast } from "@/components/ui/toast";
 import { snippetSchema } from "@/schemas/snippetSchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Editor } from "@monaco-editor/react";
 import axios, { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-import {
-    Field,
-    FieldError,
-    FieldGroup,
-    FieldLabel,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { InputGroup, InputGroupTextarea } from "@/components/ui/input-group";
-import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { FieldError } from "@/components/ui/field";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
 
 const CreateSnippet = () => {
     const router = useRouter();
 
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [language, setLanguage] = useState("plaintext");
 
     const form = useForm<z.infer<typeof snippetSchema>>({
         resolver: zodResolver(snippetSchema),
@@ -48,6 +37,8 @@ const CreateSnippet = () => {
 
     const onSubmit = async (data: z.infer<typeof snippetSchema>) => {
         setIsSubmitting(true);
+
+        console.log(data);
 
         try {
             const response = await axios.post("/api/snippets", data);
@@ -75,101 +66,97 @@ const CreateSnippet = () => {
     };
 
     return (
-        <div className="min-h-full w-full flex justify-center items-center overflow-y-auto py-8 px-4">
-            <Card className="w-full sm:max-w-md">
-                <CardHeader>
-                    <CardTitle>New Snippet</CardTitle>
-                    <CardDescription>
-                        Enter your snippet details here to create a new snippet.
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form
-                        id="snippet-form"
-                        onSubmit={form.handleSubmit(onSubmit)}
-                    >
-                        <FieldGroup>
+        <div className="h-full w-full flex flex-col justify-center overflow-y-hidden items-center py-8 px-4">
+            <h1 className="text-2xl md:text-3xl my-2 underline underline-offset-4 font-bold">Create New Snippet</h1>
+            <form
+                className="h-full w-full"
+                id="snippet-form"
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <div className="h-full w-full flex">
+                    <div className="h-full w-full flex flex-col text-xl gap-2 font-medium">
+                        <div className="w-full flex justify-between items-center flex-wrap gap-2">
                             <Controller
                                 name="title"
                                 control={form.control}
                                 render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="snippet-form-title">
-                                            Title
-                                        </FieldLabel>
+                                    <div className="flex items-center gap-2">
+                                        <Label htmlFor="snippet-form-title">
+                                            Title:
+                                        </Label>
                                         <Input
                                             {...field}
                                             id="snippet-form-title"
                                             aria-invalid={fieldState.invalid}
-                                            placeholder="REST API Response Snippet"
-                                            autoComplete="off"
+                                            type="text"
+                                            placeholder="Untitled Snippet"
                                         />
-                                        {fieldState.invalid && (
-                                            <FieldError
-                                                errors={[fieldState.error]}
-                                            />
-                                        )}
-                                    </Field>
-                                )}
-                            />
-                            <Controller
-                                name="content"
-                                control={form.control}
-                                render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="snippet-form-description">
-                                            Snippet
-                                        </FieldLabel>
-                                        <InputGroup>
-                                            <InputGroupTextarea
-                                                {...field}
-                                                id="snippet-form-description"
-                                                placeholder="export class ApiResponse {}"
-                                                className="min-h-24"
-                                                style={{
-                                                    fieldSizing: "content",
-                                                }}
-                                                aria-invalid={
-                                                    fieldState.invalid
-                                                }
-                                            />
-                                        </InputGroup>
-                                        {fieldState.invalid && (
-                                            <FieldError
-                                                errors={[fieldState.error]}
-                                            />
-                                        )}
-                                    </Field>
+                                    </div>
                                 )}
                             />
                             <Controller
                                 name="language"
                                 control={form.control}
                                 render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="snippet-form-language">
-                                            Language
-                                        </FieldLabel>
+                                    <div className="flex gap-2 justify-end items-center">
+                                        <Label htmlFor="snippet-form-language">
+                                            Language:{" "}
+                                        </Label>
                                         <Input
+                                            className="w-full"
                                             {...field}
                                             id="snippet-form-language"
                                             aria-invalid={fieldState.invalid}
-                                            placeholder="JavaScript"
-                                            autoComplete="off"
+                                            type="text"
+                                            onChange={(e) => {
+                                                field.onChange(e.target.value);
+                                                setLanguage(e.target.value);
+                                            }}
+                                            placeholder="cpp/csharp (use lowercase)"
                                         />
                                         {fieldState.invalid && (
                                             <FieldError
                                                 errors={[fieldState.error]}
                                             />
                                         )}
-                                    </Field>
+                                    </div>
                                 )}
                             />
+                        </div>
+                        <Controller
+                            name="content"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <div className="h-full max-h-100 lg:max-h-125 w-full">
+                                    <Editor
+                                        {...field}
+                                        height="100%"
+                                        width="100%"
+                                        defaultLanguage={language.toLowerCase().trim()}
+                                        onChange={(e) => {
+                                            field.onChange(e);
+                                        }}
+                                        language={language.toLowerCase().trim()}
+                                        options={{
+                                            automaticLayout: true,
+                                            minimap: { enabled: false },
+                                            scrollBeyondLastLine: false,
+                                        }}
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError
+                                            errors={[fieldState.error]}
+                                        />
+                                    )}
+                                </div>
+                            )}
+                        />
+                        <div className="flex items-center justify-between flex-wrap gap-2">
                             <Controller
                                 name="burnAfterRead"
                                 control={form.control}
-                                render={({ field }) => (
-                                    <Field>
+                                render={({ field, fieldState }) => (
+                                    <div className="flex items-center gap-2">
                                         <Label htmlFor="snippet-form-burn">
                                             Burn After Read
                                         </Label>
@@ -180,55 +167,61 @@ const CreateSnippet = () => {
                                                 field.onChange(checked)
                                             }
                                         />
-                                    </Field>
+                                        {fieldState.invalid && (
+                                            <FieldError
+                                                errors={[fieldState.error]}
+                                            />
+                                        )}
+                                    </div>
                                 )}
                             />
                             <Controller
                                 name="expiresAt"
                                 control={form.control}
                                 render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="snippet-form-expires">
-                                            Expires At
-                                        </FieldLabel>
+                                    <div className="flex items-center gap-2">
+                                        <Label
+                                            htmlFor="snippet-form-expiry"
+                                            className="w-fit"
+                                        >
+                                            Expiry (hours):
+                                        </Label>
                                         <Input
-                                            type="number"
                                             {...field}
-                                            id="snippet-form-expires"
+                                            id="snippet-form-expiry"
                                             aria-invalid={fieldState.invalid}
+                                            type="number"
                                         />
                                         {fieldState.invalid && (
                                             <FieldError
                                                 errors={[fieldState.error]}
                                             />
                                         )}
-                                    </Field>
+                                    </div>
                                 )}
                             />
-                        </FieldGroup>
-                    </form>
-                </CardContent>
-                <CardFooter>
-                    <Field orientation="horizontal">
-                        <Button
-                            type="button"
-                            variant="outline"
-                            onClick={() => form.reset()}
-                        >
-                            Reset Template
-                        </Button>
-                        <Button
-                            type="submit"
-                            form="snippet-form"
-                            disabled={isSubmitting}
-                        >
-                            {isSubmitting
-                                ? "Creating Snippet..."
-                                : "Create Snippet"}
-                        </Button>
-                    </Field>
-                </CardFooter>
-            </Card>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div className="w-full flex justify-center items-center my-4 gap-2">
+                <Button
+                    type="reset"
+                    form="snippet-form"
+                    onClick={() => form.reset()}
+                    disabled={isSubmitting}
+                    variant="outline"
+                >
+                    Reset <RefreshCcw />
+                </Button>
+                <Button
+                    type="submit"
+                    form="snippet-form"
+                    disabled={isSubmitting}
+                >
+                    Create Snippet
+                </Button>
+            </div>
         </div>
     );
 };
